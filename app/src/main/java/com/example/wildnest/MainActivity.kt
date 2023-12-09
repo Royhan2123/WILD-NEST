4cd07645581ec3884c7f2f402de58f83202f960d
Royhan <royhankeren12@gmail.com>
2023-12-09 16:32:01 +0700

    fitur camera

--- app/src/main/java/com/example/wildnest/MainActivity.kt
+++ app/src/main/java/com/example/wildnest/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.wildnest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wildnest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.container.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+           R.id.container -> {
+               val intent = Intent(this,CameraActivity::class.java)
+               startActivity(intent)
+           }
+        }
     }
 }
