ca1966eed18944273d461c024f862abb9261d33e
Royhan <royhankeren12@gmail.com>
2023-12-09 15:24:48 +0700

    folder

---
+++ app/src/main/java/com/example/wildnest/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.wildnest
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    private val timer:Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },timer)
+    }
+}
