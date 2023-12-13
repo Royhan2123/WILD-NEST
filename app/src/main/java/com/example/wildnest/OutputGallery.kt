0b2ff6077f2376697af1c79c73963539efa34f2c
Royhan <royhankeren12@gmail.com>
2023-12-13 10:01:17 +0700

    assets data

--- app/src/main/java/com/example/wildnest/OutputGallery.kt
+++ app/src/main/java/com/example/wildnest/OutputGallery.kt
@@ -2,7 +2,6 @@ package com.example.wildnest
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
