1de135eea7dddf7a406a1e65ddfe51d0d109779f
Royhan <royhankeren12@gmail.com>
2023-12-20 20:02:28 +0700

    login & register -done

--- app/src/main/java/com/example/wildnest/LoginScreen.kt
+++ app/src/main/java/com/example/wildnest/LoginScreen.kt
@@ -1,14 +1,20 @@
 package com.example.wildnest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wildnest.databinding.ActivityLoginScreenBinding
 
 @Suppress("DEPRECATION")
-class LoginScreen : AppCompatActivity() {
+class LoginScreen : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding:ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.txtSignUp.setOnClickListener(this)
     }
 
     @Deprecated("Deprecated in Java")
@@ -19,5 +25,19 @@ class LoginScreen : AppCompatActivity() {
             R.anim.fade_in,
             R.anim.fade_out,
         )
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.txtSignUp -> {
+                val intent = Intent(this@LoginScreen,RegisterScreen::class.java)
+                startActivity(intent)
+
+                this@LoginScreen.overridePendingTransition(
+                    R.anim.fade_in,
+                    R.anim.fade_out,
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/wildnest/RegisterScreen.kt
+++ app/src/main/java/com/example/wildnest/RegisterScreen.kt
@@ -1,11 +1,42 @@
 package com.example.wildnest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wildnest.databinding.ActivityRegisterScreenBinding
 
-class RegisterScreen : AppCompatActivity() {
+@Suppress("DEPRECATION")
+class RegisterScreen : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding:ActivityRegisterScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_screen)
+        binding = ActivityRegisterScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtSignIn.setOnClickListener(this)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        this@RegisterScreen.overridePendingTransition(
+            R.anim.fade_in,
+            R.anim.fade_out,
+        )
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.txtSignIn -> {
+                val intent = Intent(this@RegisterScreen,LoginScreen::class.java)
+                startActivity(intent)
+
+                this@RegisterScreen.overridePendingTransition(
+                    R.anim.fade_in,
+                    R.anim.fade_out,
+                )
+            }
+        }
     }
 }
