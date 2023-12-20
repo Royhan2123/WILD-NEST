9214f80fc36ab0cb83fbfd14c85c08ae7222fd97
Royhan <royhankeren12@gmail.com>
2023-12-20 15:12:02 +0700

    splash-scren2

---
+++ app/src/main/java/com/example/wildnest/SplashScreen2.kt
@@ -0,0 +1,11 @@
+package com.example.wildnest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen2)
+    }
+}
